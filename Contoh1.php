ca85b3ce821e250ce9e568980df91b0f2ec7ca1e
jtdewo <113327951+jtdewo@users.noreply.github.com>
2022-09-14 12:10:36 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo" Nama saya Jati Sadewo
+        saya tinggal di daerah Bekasih Timur
+        olahraga yang saya sukai adalah bulutangkis";
+    }
+}
+
+?>
